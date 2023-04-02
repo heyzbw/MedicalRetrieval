8d8d6ccdedd4abba3558d57da5cbbbab1bce4a16
Pysoible <72843164+Pysoible@users.noreply.github.com>
2023-04-02 14:40:12 +0800

    文件上传、多文件上传、根据图片生成扫描件

---
+++ WebDesign/frontend/src/api/maintainCondition.js
@@ -0,0 +1,39 @@
+conditions: [{
+    value: 'like',
+    label: '类似 like'
+}, {
+    value: 'not like',
+    label: '非类似 like'
+}, {
+    value: '=',
+    label: '等于='
+}, {
+    value: '<',
+    label: '小于<'
+}, {
+    value: '>',
+    label: '大于>'
+}, {
+    value: '<=',
+    label: '小于等于<='
+}, {
+    value: '>=',
+    label: '大于等于>='
+}, {
+    value: '<>',
+    label: '不等于<>'
+}, {
+    value: 'between',
+    label: '介于between'
+}, {
+    value: 'not between',
+    label: '非介于not between'
+}, {
+    value: 'in',
+    label: '于范围内in'
+}, {
+    value: 'not in',
+    label: '非于范围内not in'
+}]
+
+
