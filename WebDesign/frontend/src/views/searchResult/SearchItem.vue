f5b16b132302c3ddcab8f9332fa3212d45c7813e
Pysoible <72843164+Pysoible@users.noreply.github.com>
2023-04-03 20:50:10 +0800

    合并节点

--- WebDesign/frontend/src/views/searchResult/SearchItem.vue
+++ WebDesign/frontend/src/views/searchResult/SearchItem.vue
@@ -43,7 +43,8 @@
                     <p v-html="esSearchContentList[2].contentHighLight[0]"></p>
                     <hr style="height:1px;border:none;border-top:1px solid lightgray;">
                 </div>
-                <div v-if="this.ocrshow" @click="getDocView3()">【第{{ ocrResultList[0].pdfPage + 1 }}页】 <div style="color:red">
+                <div v-if="this.ocrshow" @click="getDocView3()">【第{{ ocrResultList[0].pdfPage + 1 }}页】 <div
+                        style="color:red">
                         来源于图片</div>
                     <p v-html="ocrResultList[0].ocrText"></p>
                     <hr style="height:1px;border:none;border-top:1px solid lightgray;">
